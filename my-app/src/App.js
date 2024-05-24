fe52aa7e93969b95307af44e20b2bebd0b4ce7f5
Pavel Soxanevich <soxanevich@mail.ru>
2024-05-24 14:15:19 +0300

    add comments and current year

---
+++ my-app/src/App.js
@@ -0,0 +1,26 @@
+// Декларативный
+import logo from './logo.svg';
+import './App.css';
+const currentYear = new Date().getFullYear();
+// Императивный
+export const App = () => {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+					<p>{currentYear}</p>
+				</a>
+			</header>
+		</div>
+	);
+};
